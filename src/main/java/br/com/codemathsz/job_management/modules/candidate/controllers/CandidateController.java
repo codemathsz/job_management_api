c322994d55350b38225512dd3238d7df103ed0e5
codemathsz <mo284891@gmail.com>
2025-01-07 16:02:28 -0300

    feat: Handling exception response

--- src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
@@ -6,6 +6,7 @@ import br.com.codemathsz.job_management.modules.candidate.CandidateRepository;
 import br.com.codemathsz.job_management.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,12 @@ public class CandidateController {
     private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
-        return this.createCandidateUseCase.execute(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+        try{
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
