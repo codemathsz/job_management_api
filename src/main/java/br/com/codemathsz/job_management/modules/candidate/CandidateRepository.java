3b2ed4a8890d83b5081d74e8817d765397767a46
codemathsz <mo284891@gmail.com>
2025-01-07 15:35:23 -0300

    feat: cadidateRepository

---
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/CandidateRepository.java
@@ -0,0 +1,7 @@
+package br.com.codemathsz.job_management.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> { }
