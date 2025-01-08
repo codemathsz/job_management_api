0c15998b9285dfa753a7f264fd45d4cce01219c2
codemathsz <mo284891@gmail.com>
2025-01-08 13:23:40 -0300

    feat: crypto password when create company

--- src/main/java/br/com/codemathsz/job_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/codemathsz/job_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import br.com.codemathsz.job_management.exceptions.UserFoundException;
 import br.com.codemathsz.job_management.modules.company.entities.CompanyEntity;
 import br.com.codemathsz.job_management.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,17 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity){
         this.repository
         .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
         .ifPresent((user) -> {
             throw new UserFoundException();
         });
+        // crypto password
+        companyEntity.setPassword(passwordEncoder.encode(companyEntity.getPassword()));
         return this.repository.save(companyEntity);
     }
 }
