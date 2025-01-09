2fff7f2f553c493aff54bc153eeda956c7ada045
codemathsz <mo284891@gmail.com>
2025-01-09 01:17:08 -0300

    feat: crypto candidate password

--- src/main/java/br/com/codemathsz/job_management/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.codemathsz.job_management.exceptions.UserFoundException;
 import br.com.codemathsz.job_management.modules.candidate.CandidateEntity;
 import br.com.codemathsz.job_management.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity){
         this.repository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        candidateEntity.setPassword(passwordEncoder.encode(candidateEntity.getPassword()));
+
         return this.repository.save(candidateEntity);
     }
 }
