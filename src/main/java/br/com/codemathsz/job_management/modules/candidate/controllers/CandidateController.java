4c4eb3de9c15e56deb8454bbc937c524c59cb0c6
codemathsz <mo284891@gmail.com>
2025-01-17 13:59:46 -0300

    feat: Creating a job search route and defining structure in Swagger

--- src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
@@ -4,7 +4,17 @@ import br.com.codemathsz.job_management.exceptions.UserFoundException;
 import br.com.codemathsz.job_management.modules.candidate.CandidateEntity;
 import br.com.codemathsz.job_management.modules.candidate.CandidateRepository;
 import br.com.codemathsz.job_management.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.codemathsz.job_management.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.codemathsz.job_management.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.codemathsz.job_management.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,6 +34,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
@@ -44,5 +58,23 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponiveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponiveis baseada nos filtros")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                @Content(
+                    array = @ArraySchema(schema = @Schema(
+                        implementation = JobEntity.class
+                    ))
+                )
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter){
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
--- src/main/java/br/com/codemathsz/job_management/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/codemathsz/job_management/modules/company/repositories/JobRepository.java
@@ -1,13 +1,12 @@
 package br.com.codemathsz.job_management.modules.company.repositories;
 
-import br.com.codemathsz.job_management.modules.company.entities.CompanyEntity;
 import br.com.codemathsz.job_management.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
 
-
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 }
