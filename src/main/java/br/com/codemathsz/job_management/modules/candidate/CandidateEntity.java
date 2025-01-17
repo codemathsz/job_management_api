4978cae7c4e9f2a85eda445d3cd0b407d1ba4756
codemathsz <mo284891@gmail.com>
2025-01-17 15:16:39 -0300

    feat: refactor swagger documentation

--- src/main/java/br/com/codemathsz/job_management/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/CandidateEntity.java
@@ -1,5 +1,6 @@
 package br.com.codemathsz.job_management.modules.candidate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,13 +21,18 @@ public class CandidateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Schema(example = "Matheus de Paula", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    @Schema(example = "codemathsz", requiredMode = Schema.RequiredMode.REQUIRED)
     private String username;
     @Email(message = "O campo [email] deve conter um email válido")
+    @Schema(example = "matheus@example.com", requiredMode = Schema.RequiredMode.REQUIRED)
     private String email;
     @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+    @Schema(example = "Admin1234@", minLength = 10, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED)
     private String password;
+    @Schema(example = "Desenvolvedor Java", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
     private String curriculum;
 
--- src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/codemathsz/job_management/modules/candidate/controllers/CandidateController.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 
     @Autowired
@@ -40,6 +41,17 @@ public class CandidateController {
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
+    @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                schema = @Schema(
+                        implementation = CandidateEntity.class
+                )
+            )
+        }),
+        @ApiResponse(responseCode = "400", description = "Usuario já existe")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
         try{
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -51,7 +63,6 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
@@ -76,7 +87,6 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Listagem de vagas disponiveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponiveis baseada nos filtros")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
