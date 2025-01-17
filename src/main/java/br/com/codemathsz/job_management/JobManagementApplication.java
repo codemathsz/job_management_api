4d4c2936f2c3c99f057402e1cdf661505c287f69
codemathsz <mo284891@gmail.com>
2025-01-17 13:20:48 -0300

    feat: adding and configuring swagger

--- src/main/java/br/com/codemathsz/job_management/JobManagementApplication.java
+++ src/main/java/br/com/codemathsz/job_management/JobManagementApplication.java
@@ -1,9 +1,18 @@
 package br.com.codemathsz.job_management;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Job Management",
+				description = "API responsible for job management",
+				version = "1"
+		)
+)
 public class JobManagementApplication {
 
 	public static void main(String[] args) {
